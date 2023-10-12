91f07bd3ec9505c1adc81f0d4e4e8555945193f0
boutaina <rh.boutaina@gmail.com>
2023-10-12 13:13:42 -0700

    update

--- models/user.py
+++ models/user.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+""" Define the user class """
 from models.base_model import BaseModel
+""" represente un utilisateur
+les attributs :
+    email(str): l'email de user
+    password(str): password de user
+    first_name(str): le prénom de user
+    last_name (str): le nom de user
+"""
 
 class User(BaseModel):
     email = ""
