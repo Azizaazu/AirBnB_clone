9a51fe7d2af18b16dd9e88194ab224ec3945a7d9
Azizaazu <aynaou.aziza98@gmail.com>
2023-10-13 15:14:54 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,37 @@
+#!/usr/bin/python3
+""" module containing FileStorage used for file storage"""
 from models.user import User
 from models.base_model import BaseModel
 
 class FileStorage:
-    # ...
-    def all(self, cls=None):
-        """Return a dictionary of objects."""
-        obj_dict = {}
-        if cls:
-            for key, value in self.__objects.items():
-                if type(value) == cls:
-                    obj_dict[key] = value
-        else:
-            obj_dict = self.__objects
-        return obj_dict
+    """ serializes and deserializes instances to and from JSON file
+    saved into file_path"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        """ creates a new object and saves it to __objects"""
+        key = "{}.{}".format(obj.__class__.____name, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+         """update the JSON file to reflect any change in the objects"""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+         """ update __objects dict"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, obj_data in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    obj_class = models[class_name]
+                    self.__objects[key] = obj_class(**obj_data)
+        except FileNotFoundError:
+            pass
