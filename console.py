d9378d4d6c30ba9517c0617efe7c5944cd16f7c4
Azizaazu <aynaou.aziza98@gmail.com>
2023-10-15 13:48:59 -0700

    update

--- console.py
+++ console.py
@@ -5,15 +5,15 @@
 
 import cmd
 import json
-import os
+import shlex
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from models.user import User
-from models.place import Place
-from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.user import User
+from models.place import Place
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the entry point of the command interpreter.
